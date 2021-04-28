ff0681cfaa25e7eb7509e3ee03331a6d55fecce1
Petukhov Leonid <rusadrako@yandex.ru>
2021-04-28 16:05:21 +0300

    Класс folder: exists_control(string $folder) - Проверяет существование указанной папки и создаёт при отсутствии + тест

--- src/folder.php
+++ src/folder.php
@@ -19,40 +19,21 @@ class folder {
 
 
 
-	/** Чистит путь к папке
-	 * @param string $folder Полный путь директории
-	 */
-	public function clean($folder) {
-		# Разворачиваем все обратные слэши
-		$folder = \str_replace('\\', '/', $folder);
-		# Чистим сдвоенные слэши
-		$folder = preg_replace('/(\/)+/ui', '/', $folder);
-		# Если есть закрывающий слэш - удаляем
-		if ($folder[-1] === '/') {
-			$folder = \function_exists('mb_substr')
-					? \mb_substr($folder, 0, -1)
-					: \substr($folder, 0, -1);
-		}
-		return $folder;
-	}
-
-
-
 	/** Проверяет существование указанной папки и создаёт при отсутствии
 	 * @param string $folder Полный путь директории
 	 */
-	public function exists_control($folder) {
-		$arr_folder = \explode('/', $this->clean($folder));
+	public function exists_control(string $folder) {
+		$arr_folder = \explode('/', $this->_obj_disc->clean_name($folder));
 		$_folder = '';
 		foreach($arr_folder as $k => $v) {
 			$_folder = $_folder . $v . '/';
-			if (!\file_exists($_folder)) {
+			if (!\file_exists($_folder) && !\is_dir($_folder)) {
 				if (!\mkdir($_folder, 0777)) {
 					return false;
 				};
 			}
 		}
-		return true;
+		return $_folder;
 	}
 
 
--- test/folder_Test.php
+++ test/folder_Test.php
@@ -32,29 +32,17 @@ class folder_Test extends TestCase {
 		$folder = __DIR__;
 		$folder = \str_replace('\\', '/', $folder);
 		$arr_folder = \explode('/', $folder);
-		array_pop($arr_folder);
-		return $folder = implode('/', $arr_folder) . '/___test_folder/';
-	}
-
-
-
-	/** Проверяет чистку пути */
-	public function test_clean() {
-		$control_result = $this->_root_folder() . 'test';
-		$result = $this->_test_object->clean($control_result);
-		$this->assertEquals($result, $control_result, 'Чистка пути 1 выполнена неверно');
-		$result = $this->_test_object->clean($control_result . '/');
-		$this->assertEquals($result, $control_result, 'Чистка пути 2 выполнена неверно');
-		$result = $this->_test_object->clean($control_result . '//');
-		$this->assertEquals($result, $control_result, 'Чистка пути 3 выполнена неверно');
+		\array_pop($arr_folder);
+		return $folder = implode('/', $arr_folder) . '/test_folder';
 	}
 
 
 
 	/** Проверяет контроль существования папки */
 	public function test_exists_control() {
-		$result = $this->_test_object->exists_control($this->_root_folder() . 'test');
-		$this->assertTrue($result, 'Папка не создана');
+		$control_result = $this->_root_folder() . '/test';
+		$result = $this->_test_object->exists_control($this->_root_folder() . '/test');
+		$this->assertEquals($result, $control_result . '/', 'Папка не создана');
 	}
 
 
