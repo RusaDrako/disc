9ceaee6eb7c04a9d1719b55b56091a823cb35126
Petukhov Leonid <rusadrako@yandex.ru>
2021-04-28 12:05:36 +0300

    Набор заготовок тестов

---
+++ test/folder_Test.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../src/autoload.php');
+
+
+
+/**
+ * @author Петухов Леонид <l.petuhov@okonti.ru>
+ */
+class folder_Test extends TestCase {
+	/** Тестируемый объект */
+	private $_test_object = null;
+
+
+
+	/** Вызывается перед каждым запуском тестового метода */
+	protected function setUp() : void {
+		$this->_test_object = (new RD_Obj_Disc())->folder();
+	}
+
+
+
+	/** Вызывается после каждого запуска тестового метода */
+	protected function tearDown() : void {}
+
+
+
+	/** */
+	public function test_new() {
+		$this->assertEquals(1, 1, 'Неверное значение');
+	}
+
+
+
+/**/
+}
